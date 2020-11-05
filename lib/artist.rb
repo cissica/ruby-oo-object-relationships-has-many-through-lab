f1e5940c3f589879b44a44cc73c10de04c354e26
cissica <cecemartinez@hotmail.com>
2020-11-05 13:03:34 -0800

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+require "pry"
+class Artist 
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name 
+        save
+    end 
+
+    def save
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def songs
+        Song.all.select{|s| s.artist == self}
+    end 
+
+    def new_song(name, genre)
+        song = Song.new(name, self, genre)
+    end 
+
+    def genres
+        songs.collect {|s| s.genre}
+    end 
+
+
+end 
